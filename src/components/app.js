3a9a7a9b541c2ff7695a1e8ef2ca663fa0d977d6
Michael Zhao <michaelzhao314@gmail.com>
2021-04-17 13:06:51 -0500

    Added news section

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,38 @@
+import React from 'react';
 import Todo from './todo';
 import News from './news';
 import Time from './time';
 
 import './app.css';
 
-function App() {
-    return (
-        <div className="app">
-            <div className="app-vert">
-                <Todo className="app-left section"></Todo>
-                <div className="app-right">
-                    <Time className="app-time section"></Time>
-                    <News className="app-news section"></News>
+const BACKEND = 'http://localhost:8080';
+const USER = 'michael';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { todo: null, news: [] };
+    }
+
+    async componentDidMount() {
+        const todo = await fetch(`${BACKEND}/todo/${USER}`).then((d) => d.json());
+        const news = await fetch(`${BACKEND}/news`).then((d) => d.json());
+        this.setState({ todo, news: news.results });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <div className="app-vert">
+                    <Todo className="app-left section" data={this.state.todo}></Todo>
+                    <div className="app-right">
+                        <Time className="app-time section"></Time>
+                        <News className="app-news section" data={this.state.news}></News>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/news.js
+++ src/components/news.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Article from './article';
 import './news.css';
 
 class News extends React.Component {
+    createArticleList = () => {
+        return this.props.data.map((d, i) => {
+            return (
+                <Article
+                    title={d.title}
+                    abstract={d.abstract}
+                    byline={d.byline}
+                    url={d.url}
+                    index={i + 1}
+                    key={`${d.title}-${i}`}></Article>
+            );
+        });
+    };
+
     render() {
-        return <div className={`news ${this.props.className}`}>News Section</div>;
+        const articleList = this.createArticleList();
+        return <div className={`news ${this.props.className}`}>{articleList}</div>;
     }
 }
 
