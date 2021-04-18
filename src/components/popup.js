1713413ec2b1a79175ae016719ff4f3553c1f7cb
Michael Zhao <michaelzhao314@gmail.com>
2021-04-17 21:29:50 -0500

    Make save and cancel buttons

--- src/components/popup.js
+++ src/components/popup.js
@@ -2,21 +2,40 @@ import React from 'react';
 import './popup.css';
 
 class Popup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { disabled: false };
+    }
     cancelButton = () => {
         this.props.closePopup();
-    }
+    };
+
     saveButton = async () => {
+        this.setState({ disabled: true });
         // Save stuff
         this.props.closePopup();
-    }
+    };
+
     render() {
         const open = this.props.open ? 'open' : '';
         return (
             <div className={`popup section ${open}`}>
                 <p className="popup-title">This is a popup!</p>
                 <div className="popup-button-container">
-                    <button className="popup-cancel popup-button" onClick={this.cancelButton}>Cancel</button>
-                    <button className="popup-save popup-button" onClick={this.saveButton}>Save</button>
+                    <button className="popup-cancel popup-button" onClick={this.cancelButton}>
+                        Cancel
+                    </button>
+                    <div className={`popup-type-container 1`}>
+                        
+                    </div>
+
+
+                    <button
+                        className="popup-save popup-button"
+                        onClick={this.saveButton}
+                        disabled={this.state.disabled}>
+                        Save
+                    </button>
                 </div>
             </div>
         );
