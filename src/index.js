ae538f475a82c0a343181237b9b11637470cdb94
Michael Zhao <michaelzhao314@gmail.com>
2021-06-13 23:15:47 -0500

    Added background getting and saving

--- src/index.js
+++ src/index.js
@@ -1,33 +1,49 @@
 $(document).ready(async () => {
+    setBackground();
     startTime();
-    writeNews();    
+    writeNews();
 });
+
+/* BACKGROUND */
+async function setBackground() {
+    const savedBackground = await browser.storage.local.get('background');
+    if (savedBackground.background === undefined) {
+        getAndSaveBackground();
+        return;
+    }
+    $('#background-image').attr('src', savedBackground.background);
+}
+
+async function getAndSaveBackground() {
+    const newBackground = await fetch('https://api.michaelzhao.xyz/background/random').then((d) =>
+        d.json()
+    );
+    $('#background-image').attr('src', newBackground.imageUrl);
+    browser.storage.local.set({ background: newBackground.imageUrl });
+}
 
 /* DATE AND TIME SECTION */
 
 function startTime() {
-    setNow();
-    window.setInterval(setNow, 1000);
+    setTime();
+    window.setInterval(setTime, 1000);
 }
 
-function setNow() {
+function setTime() {
     const now = dayjs();
     const time = now.format('hh:mm:ss a');
     const date = now.format('dddd, MMMM DD, YYYY');
-    setTime(time, date);
-}
-
-function setTime(time, date) {
-    $('.time-time').text(time);
-    $('.time-date').text(date);
+    $('.time').text(time);
+    $('.date').text(date);
 }
 
 /* NEWS SECTION */
 
-function writeNews() {
-    let articleList = $('.news');
+async function writeNews() {
+    const news = await fetch('https://api.michaelzhao.xyz/news').then((d) => d.json());
+    let articleList = $('#news');
     articleList.empty();
-    window.state.news.forEach((n, i) => {
+    news.results.forEach((n, i) => {
         let subtitle = n.abstract;
         if (subtitle === '') subtitle = n.byline;
         let article = $(`<a class="article" href="${n.url}"></a>`);
