21d2eb0192aeb9e2b59aa56d2432edc87c53a312
Michael Zhao <michaelzhao314@gmail.com>
2021-06-14 17:16:33 -0500

    Changed name of built zip file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "bundling app into zip..."
-rm task-tab.zip
-zip -r task-tab.zip LICENSE.md manifest.json README.md lib res src
+rm atab.zip
+zip -r atab.zip LICENSE.md manifest.json README.md lib res src
 echo "finished bundling!"
