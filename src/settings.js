c321a46fada47329f8b8a7456e6823a7711eeb1b
Michael Zhao <michaelzhao314@gmail.com>
2021-08-17 19:36:02 -0500

    Added setting to scale view

--- src/settings.js
+++ src/settings.js
@@ -3,9 +3,11 @@ import colors from './files/colors.json';
 import './css/settings.css';
 
 export async function createSettings() {
-    const data = await browser.storage.sync.get('color');
+    const data = await browser.storage.sync.get(['color', 'scale']);
     const prevId = data.color ? data.color : 0;
+    const prevScale = data.scale ? data.scale : 100;
     setColor(prevId);
+    scaleView(prevScale);
 
     const gradientList = colors.map((c) =>
         $('<div>')
@@ -15,12 +17,18 @@ export async function createSettings() {
             .css('--end', c.end)
     );
 
-    const formWrapper = $('<div>').attr('id', 'form-wrapper').append(gradientList);
+    const scaleLabel = $('<p>').attr('id', 'scale-label').text('Scale (%)');
+    const scale = $('<input>').attr('id', 'scale').val(prevScale).on('blur', changeScale);
+    const scaleError = $('<p>').addClass('error').attr('id', 'scale-error');
+
+    const formWrapper = $('<div>')
+        .attr('id', 'form-wrapper')
+        .append([...gradientList, scaleLabel, scale, scaleError]);
     $('#settings-wrapper').empty().append(formWrapper);
 }
 
 async function changeColor(id) {
-    setColor(id)
+    setColor(id);
     await browser.storage.sync.set({ color: id });
     createSettings();
 }
@@ -30,6 +38,17 @@ function setColor(id) {
     $('body').css('--start', color.start).css('--end', color.end);
 }
 
-export async function createSidebar() {
-    
+function scaleView(scale) {
+    $('html').css('font-size', `${Number(scale) / 100 * 16}px`);
 }
+
+async function changeScale(event) {
+    $('#scale-error').empty();
+    if (isNaN(Number(event.target.value))) $('#scale-error').text('Please enter a number');
+    else {
+        browser.storage.sync.set({ scale: event.target.value });
+        scaleView(event.target.value);
+    }
+}
+
+export async function createSidebar() {}
